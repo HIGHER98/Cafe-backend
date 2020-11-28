dcdc9390bae99cdf5d2416f957335574933bcf7a
HIGHER98 <gary.james.hyer@gmail.com>
2020-11-28 20:07:50 +0000

    Moved item related things from admin.go to a new file

--- routers/api/admin/admin.go
+++ routers/api/admin/admin.go
@@ -12,94 +12,7 @@ import (
 	"gorm.io/gorm"
 )
 
-type Item struct {
-	Name        string  `form:"name"`
-	Description string  `form:"description"`
-	Price       float32 `form:"price"`
-	Category    int     `form:"category"`
-	Tag         int     `form:"tag"`
-}
-
-//Get all live items
-func GetItems(c *gin.Context) {
-	appG := app.Gin{C: c}
-	items, err := models.GetItemsForSale()
-	if err != nil {
-		appG.Response(http.StatusInternalServerError, e.ERROR, nil)
-		return
-	}
-	appG.Response(http.StatusOK, e.SUCCESS, items)
-}
-
-//Update specific item
-func UpdateItem(c *gin.Context) {
-	appG := app.Gin{C: c}
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		appG.Response(http.StatusBadRequest, e.BAD_REQUEST, nil)
-		return
-	}
-
-	var item models.Item
-	err = c.Bind(&item)
-	if err != nil {
-		appG.Response(http.StatusBadRequest, e.BAD_REQUEST, nil)
-		return
-	}
-
-	err = models.UpdateItem(id, item)
-	if err == gorm.ErrRecordNotFound {
-		appG.Response(http.StatusOK, e.ID_NOT_FOUND, nil)
-		return
-	} else if err != nil {
-		appG.Response(http.StatusInternalServerError, e.ERROR, nil)
-		logging.Error(err)
-		return
-	}
-	appG.Response(http.StatusOK, e.SUCCESS, nil)
-}
-
-//Add item to the menu
-func AddItem(c *gin.Context) {
-	appG := app.Gin{C: c}
-	var item models.Item
-	err := c.Bind(&item)
-	if err != nil {
-		appG.Response(http.StatusBadRequest, e.BAD_REQUEST, nil)
-		return
-	}
-
-	logging.Debug("Adding item: ", item)
-	err = models.AddItem(item)
-	if err != nil {
-		appG.Response(http.StatusInternalServerError, e.ERROR, nil)
-		logging.Warn("Failed to create item: ", item, "\nError: ", err)
-		return
-	}
-	appG.Response(http.StatusCreated, e.CREATED, nil)
-}
-
-func DelItem(c *gin.Context) {
-	appG := app.Gin{C: c}
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		appG.Response(http.StatusBadRequest, e.BAD_REQUEST, nil)
-		return
-	}
-	err = models.SetItemIsDel(id)
-	if err == gorm.ErrRecordNotFound {
-		appG.Response(http.StatusBadRequest, e.ID_NOT_FOUND, nil)
-		return
-	} else if err != nil {
-		appG.Response(http.StatusInternalServerError, e.ERROR, nil)
-		logging.Error(err)
-		return
-	}
-	appG.Response(http.StatusOK, e.DELETED, nil)
-}
-
 //VIEW/EDIT/DELETE/ADD staff
-
 func GetUsers(c *gin.Context) {
 	appG := app.Gin{C: c}
 	users, err := models.GetActiveUsers()
@@ -110,7 +23,7 @@ func GetUsers(c *gin.Context) {
 	appG.Response(http.StatusOK, e.SUCCESS, users)
 }
 
-//Sets user.is_del=0
+//Sets is_del=0
 func DelUser(c *gin.Context) {
 	appG := app.Gin{C: c}
 	id, err := strconv.Atoi(c.Param("id"))
